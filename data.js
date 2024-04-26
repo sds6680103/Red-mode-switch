13bbbbcc0f7aae8c1a7102fb90afc71172429652
sds6680103 <166886092+sds6680103@users.noreply.github.com>
2024-04-26 19:28:20 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+const articles = [
+  {
+    id: 1,
+    title: 'New coffee machine',
+    date: new Date(2024, 1, 21),
+    length: 5,
+    snippet: `New coffee machine installed in the restroom. Free to enjoy more coffee.`,
+  },
+  {
+    id: 2,
+    title: 'Rooftop Garden',
+    date: new Date(2024, 2, 24),
+    length: 11,
+    snippet: `Rooftop Garden construced. Whenever you are stuffed by burdonsome of workload, come anytime you want and enjoy the green oxygen.`,
+  },
+  {
+    id: 3,
+    title: 'Stock option to workers',
+    date: new Date(2024, 3, 26),
+    length: 7,
+    snippet: `100 stocks were newly published and workers are able to buy at lowered price of 5 dollors per one stock.`,
+  },
+  {
+    id: 4,
+    title: 'Security Manual renewal',
+    date: new Date(2024, 3, 28),
+    length: 7,
+    snippet: `As scurity issues ariesed among our company's competitors, advanced scurity manual was completed and will be realeased to every workers. Be sure to read and keep in mind.`,
+  },
+];
